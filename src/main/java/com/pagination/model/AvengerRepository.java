3b91c22c7b46fe03e708cd21959b8356fcd946d6
Rasheed <shaikrasheed3014@gmail.com>
2022-11-13 23:31:46 +0530

    Add. Avenger repository

---
+++ src/main/java/com/pagination/model/AvengerRepository.java
@@ -0,0 +1,6 @@
+package com.pagination.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AvengerRepository extends JpaRepository<Avenger, Integer> {
+}
---
+++ src/test/java/com/pagination/model/AvengerRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.pagination.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class AvengerRepositoryTest {
+    @Autowired
+    private AvengerRepository avengerRepository;
+    private Avenger ironman;
+
+    @BeforeEach
+    void setUp() {
+        ironman = new Avenger("Ironman", "Iron Man", 2008);
+        avengerRepository.save(ironman);
+    }
+
+    @Test
+    void shouldBeAbleToSaveAvengerDetails() {
+        Avenger savedIronman = avengerRepository.save(ironman);
+
+        assertEquals(savedIronman.getId(), ironman.getId());
+        assertEquals(savedIronman.getName(), ironman.getName());
+        assertEquals(savedIronman.getIntroducedInMovie(), ironman.getIntroducedInMovie());
+    }
+
+    @Test
+    void shouldBeAbleToGetAvengerDetailsById() {
+        Avenger avenger = avengerRepository.findById(ironman.getId()).get();
+
+        assertEquals(avenger.getId(), ironman.getId());
+        assertEquals(avenger.getName(), ironman.getName());
+        assertEquals(avenger.getIntroducedInYear(), ironman.getIntroducedInYear());
+        assertEquals(avenger.getIntroducedInMovie(), ironman.getIntroducedInMovie());
+    }
+}
