cc4d333fd225c8cd13fbddd6e842b5fac7d68484
Rasheed <shaikrasheed3014@gmail.com>
2022-11-14 11:39:25 +0530

    Add. Endpoint to get list of avengers with pagination

--- src/main/java/com/pagination/controller/AvengerController.java
+++ src/main/java/com/pagination/controller/AvengerController.java
@@ -2,9 +2,12 @@ package com.pagination.controller;
 
 import com.pagination.model.Avenger;
 import com.pagination.model.AvengerRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,6 +23,19 @@ public class AvengerController {
     @GetMapping("/avengers")
     public ResponseEntity<?> avengers() {
         List<Avenger> avengers = avengerRepository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(avengers);
+    }
+
+    @GetMapping("/avengers-with-pagination")
+    public ResponseEntity<?> avengersWithPagination(
+            @RequestParam(value = "pageNumber", defaultValue = "0", required = true) int pageNumber,
+            @RequestParam(value = "pageSize", defaultValue = "10", required = true) int pageSize
+    ) {
+        PageRequest pageRequest = PageRequest.of(pageNumber, pageSize);
+        Page<Avenger> avengersPage = avengerRepository.findAll(pageRequest);
+
+        List<Avenger> avengers = avengersPage.getContent();
+
         return ResponseEntity.status(HttpStatus.OK).body(avengers);
     }
 }
--- src/test/java/com/pagination/controller/AvengerControllerTest.java
+++ src/test/java/com/pagination/controller/AvengerControllerTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -16,6 +19,7 @@ import java.util.List;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = AvengerController.class)
@@ -25,12 +29,16 @@ public class AvengerControllerTest {
     @MockBean
     private AvengerRepository avengerRepository;
     private List<Avenger> avengers;
+    private PageRequest pageRequest;
+    private Page<Avenger> avengersPage;
 
     @BeforeEach
     void setUp() {
         Avenger ironman = new Avenger("Ironman", "Iron Man", 2008);
         Avenger thor = new Avenger("Thor", "Thor", 2011);
         avengers = Arrays.asList(ironman, thor);
+        pageRequest = PageRequest.of(0, 1);
+        avengersPage = new PageImpl<>(avengers);
     }
 
     @Test
@@ -42,5 +50,23 @@ public class AvengerControllerTest {
         result.andExpect(status().isOk()).andDo(print());
 
         verify(avengerRepository, times(1)).findAll();
+    }
+
+    @Test
+    void shouldBeAbleToReturnListOfAvengersWithPagination() throws Exception {
+        when(avengerRepository.findAll(pageRequest)).thenReturn(avengersPage);
+
+        ResultActions result = mockMvc.perform(get("/avengers-with-pagination?pageNumber=0&pageSize=1"));
+
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].id").value(avengers.get(0).getId()))
+                .andExpect(jsonPath("$.[0].name").value(avengers.get(0).getName()))
+                .andExpect(jsonPath("$.[0].introducedInMovie").value(avengers.get(0).getIntroducedInMovie()))
+                .andExpect(jsonPath("$.[1].id").value(avengers.get(1).getId()))
+                .andExpect(jsonPath("$.[1].name").value(avengers.get(1).getName()))
+                .andExpect(jsonPath("$.[1].introducedInYear").value(avengers.get(1).getIntroducedInYear()))
+                .andDo(print());
+
+        verify(avengerRepository, times(1)).findAll(pageRequest);
     }
 }
