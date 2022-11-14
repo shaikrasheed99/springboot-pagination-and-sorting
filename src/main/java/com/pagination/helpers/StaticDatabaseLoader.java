dc1f2fb98e3029c5acbddde805e9f953383e3591
Rasheed <shaikrasheed3014@gmail.com>
2022-11-14 10:31:34 +0530

    Refactor. Configuration of postgres database

--- src/main/java/com/pagination/helpers/StaticDatabaseLoader.java
+++ src/main/java/com/pagination/helpers/StaticDatabaseLoader.java
@@ -17,7 +17,7 @@ public class StaticDatabaseLoader {
     @Bean
     public CommandLineRunner initDatabase() {
         return args -> {
-            String[] names = {"Ironman", "Thanos", "Captain America", "Thor", "Loki", "Hulk", "Captain Marvel", "Spider Man", "Black Widow", "Hawkeye", "Black Panther", "Doctor Strange", "Ant Man", "Vision", "Hulkbuster", "War Machine", "Nick Fury", "Falcon", "Winter Soldier", "Ultron", "Red Skull" };
+            String[] names = {"Iron Man", "Thanos", "Captain America", "Thor", "Loki", "Hulk", "Captain Marvel", "Spider Man", "Black Widow", "Hawkeye", "Black Panther", "Doctor Strange", "Ant Man", "Vision", "Hulkbuster", "War Machine", "Nick Fury", "Falcon", "Winter Soldier", "Ultron", "Red Skull" };
             String[] movies = {"Iron Man", "The Avengers", "Captain America: The First Avenge", "Thor", "Thor", "The Incredible Hulk", "Captain Marvel", "Captain America: Civil War", "Iron Man 2", "Thor", "Captain America: Civil War", "Doctor Strange", "Ant Man", "Avengers: Age of Ultron", "Avengers: Age of Ultron", "Iron Man", "Iron Man", "Captain America: The Winter Soldier", "Captain America: The Winter Soldier", "Avengers: Age of Ultron", "Captain America: The First Avenger" };
             int[] years = {2008, 2012, 2011, 2011, 2011, 2008, 2019, 2016, 2010, 2015, 2016, 2016, 2015, 2015, 2015, 2008, 2008, 2014, 2014, 2015, 2011};
 
